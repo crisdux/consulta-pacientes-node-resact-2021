6c15501972cbb92b40741668e4c1ea40c1a17a2d
cris.dux <crisfer.4217@gmail.com>
2021-06-18 22:45:09 -0400

    Conexion a la base de datos con mongoose

--- API/index.js
+++ API/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const colors = require('colors')
 const morgan = require('morgan')
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
 app.set('appName',"Consulta de pacientes".toUpperCase());
+
+// conectar a base de datos
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
 app.use(morgan('dev'));
 
