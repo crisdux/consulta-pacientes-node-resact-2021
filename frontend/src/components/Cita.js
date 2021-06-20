4f9035390b88eb9c671f5defe73795fff5e8e3ea
cris.dux <crisfer.4217@gmail.com>
2021-06-20 17:15:05 -0400

    se creo el routing con react-router-dom

---
+++ frontend/src/components/Cita.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Cita = () => {
+    return ( <h1>Desde cita</h1> );
+}
+ 
+export default Cita;
---
+++ frontend/src/components/Pacientes.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Pacientes = () => {
+    return ( <h1>Desde Pacientes</h1> );
+}
+ 
+export default Pacientes;
